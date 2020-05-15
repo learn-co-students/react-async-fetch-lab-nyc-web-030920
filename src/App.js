f40c086a4f7c880fa3f7ca0ef645ae9ab0309752
CBreakr <github email address>
2020-05-15 13:50:44 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+
+import React from "react";
+
+export default class App extends React.Component {
+
+    state = {
+
+    }
+
+    componentDidMount(){
+        fetch("http://api.open-notify.org/astros.json")
+        .then(res => res.json())
+        .then(data => {
+            this.setState({astronauts: data});
+        })
+        .catch(err => console.err("Error", err));
+    }
+
+    render(){
+        return (
+            <div></div>
+        )
+    }
+}
